5585d1f1e50bfd111e2d3128e901bdccb017b7df
niuhaiyangI <95624307+niuhaiyangI@users.noreply.github.com>
2023-04-09 00:27:26 +0800

    commit

--- Service/Camera/differentiation.py
+++ Service/Camera/differentiation.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import torch
 from torchvision import transforms
 import numpy as np
@@ -9,6 +10,7 @@ class SLOT:
         self.fps=fps
         self.slot_size=slot_size
         self.Hz=1.0/(slot_size/self.fps)
+        self.heart_time=slot_size/self.fps
         # print(1.0/self.Hz)
         self.slot_list=s_list
         self.red_average=red_average
@@ -23,6 +25,7 @@ class SLOT:
         self.score=self._cal_score()
         self.W_c=self._cal_W()
         self.DN_index=0
+        self.rolling_window=4
 
     def _cal_score(self):
         score=0
@@ -37,6 +40,24 @@ class SLOT:
             temp=torch.tensor(img*self.M).cuda().sum(dim=(0,1))/torch.tensor(self.M).cuda().sum(dim=(0,1))
             W_list.append(temp.tolist())
         W_list=torch.tensor(W_list)
+        red_channel=W_list[:,2]
+        green_channel=W_list[:,1]
+        blue_channel = W_list[:,0]
+        b,a = signal.butter(10,0.3, btype='highpass',fs=self.slot_size)
+        # red_channel=signal.filtfilt(b,a,red_channel.tolist(),axis=0,padlen=self.slot_size-1)
+        # green_channel = signal.filtfilt(b, a, green_channel.tolist(),axis=0,padlen=self.slot_size-1)
+        # blue_channel = signal.filtfilt(b,a,blue_channel.tolist(),axis=0,padlen=self.slot_size-1)
+        red_channel=signal.lfilter(b,a,red_channel)
+        green_channel = signal.lfilter(b, a, green_channel)
+        blue_channel = signal.lfilter(b,a,blue_channel)
+        # sos= signal.butter(10,0.3, btype='highpass',fs=self.slot_size,output='sos')
+        # zi=signal.sosfilt_zi(sos)
+        # red_channel,_=signal.sosfilt(sos,red_channel,zi=zi)
+        # green_channel,_ = signal.sosfilt(sos, green_channel,zi=zi)
+        # blue_channel,_ = signal.sosfilt(sos,blue_channel,zi=zi)
+        W_list[:,2]=torch.tensor(red_channel.copy())
+        W_list[:,1]=torch.tensor(green_channel.copy())
+        W_list[:,0]=torch.tensor(blue_channel.copy())
         W_list.cuda()
         return W_list
 
@@ -53,6 +74,7 @@ class SLOT:
         return (x)/(self.slot_size-1)
 
     def get_Systolic_DiastolicFeature(self):
+        print(self.Hz)
         x=(torch.tensor(range(self.slot_size)))/(self.slot_size-1)
         red_channel=self.W_c[:,2]
         green_channel=self.W_c[:,1]
@@ -60,22 +82,40 @@ class SLOT:
         red_channel=(red_channel-red_channel.min())/(red_channel.max()-red_channel.min())
         green_channel = (green_channel - green_channel.min()) / (green_channel.max() - green_channel.min())
         blue_channel = (blue_channel - blue_channel.min()) / (blue_channel.max() - blue_channel.min())
-        sum=self.W_c.sum(dim=1)/3
-        sum=(sum - sum.min()) / (sum.max() - sum.min())
         plt.close()
         plt.plot(x, red_channel, color='r', label='red_average')
         plt.plot(x, green_channel, color='g', label='green_average')
         plt.plot(x, blue_channel, color='b', label='blue_average')
-        plt.plot(x, sum, color='black', label='blue_average')
         plt.show()
-        red_list=[]
-        green_list=[]
-        blue_list=[]
-        index_r=red_channel.argmax()
-        index_g=green_channel.argmax()
-        index_b=blue_channel.argmax()
 
 
-    # def get_Non_fiducialFeature(self,slot):
+    def get_Non_fiducialFeature(self):
+        print(self.heart_time)
+        x = (torch.tensor(range(self.slot_size))) / (self.slot_size - 1)
+        red_channel = self.W_c[:, 2]
+        green_channel = self.W_c[:, 1]
+        blue_channel = self.W_c[:, 0]
+        # red_channel = (red_channel - red_channel.min()) / (red_channel.max() - red_channel.min())
+        # green_channel = (green_channel - green_channel.min()) / (green_channel.max() - green_channel.min())
+        # blue_channel = (blue_channel - blue_channel.min()) / (blue_channel.max() - blue_channel.min())
+        b1,a1=signal.butter(10,1,btype='high',fs=self.slot_size)
+        b2,a2=signal.butter(10,2,btype='high',fs=self.slot_size)
+        sf_r1=signal.lfilter(b1,a1,red_channel)
+        sf_r2 = signal.lfilter(b2, a2, red_channel)
+        sf_r1_rolling=pd.Series(sf_r1).rolling(window=self.rolling_window).mean()
+        sf_r2_rolling = pd.Series(sf_r2).rolling(window=self.rolling_window).mean()
+        peak_r1,_=signal.find_peaks(-sf_r1_rolling)
+        peak_r2, _ = signal.find_peaks(-sf_r2_rolling)
+        # print(sf_r1)
+        plt.close()
+        # plt.plot(peak_r1/(self.slot_size-1), sf_r1_rolling[peak_r1], 'x', color='black')
+        # plt.plot(peak_r2/(self.slot_size-1), sf_r2_rolling[peak_r2], 'x', color='b')
+        # plt.plot(x, sf_r1_rolling, color='r', label='green_average')
+        # plt.plot(x, sf_r2_rolling, color='b', label='green_average')
+        plt.plot(x, sf_r1, color='r', label='green_average')
+        plt.plot(x, sf_r2, color='b', label='green_average')
+        plt.show()
+
+
 
 
--- Service/Camera/divide.py
+++ Service/Camera/divide.py
@@ -18,9 +18,11 @@ from Camera.differentiation import SLOT
 class divide:
     def __init__(self, cam):
         print("SLOT devide starting....")
+        self.distance=3
+        self.rolling_window=2*self.distance+1
         self.heart_pump_seconds = 0.8  ##心脏跳动时间
-        self.heart_pump_max = 1.1
-        self.heart_pump_min = 0.5
+        self.heart_pump_max = 1.3
+        self.heart_pump_min = 0.3
         self.camera = cam
         self.frames_num = int(self.camera.get(cv2.CAP_PROP_FRAME_COUNT))
         # print(self.frames_num)
@@ -40,8 +42,9 @@ class divide:
         self.pixel_num=self.img_list[0].shape[0]*self.img_list[0].shape[1]
         self.red_average = torch.zeros(self.frames_num)
         self.cal_red()
-        self.slots_list,self.slots_size,self.score_average = self.get_slots()
-        self.real_pump_time=(self.frames_num/self.fps)/self.slots_size
+        self.peak,_=signal.find_peaks(-self.red_average)
+        self.slots_list,self.slots_size,self.score_average,self.heart_pump_frames = self.get_slots()
+        self.real_pump_time=((self.heart_pump_frames)/self.fps)/self.slots_size
         self.print()
 
     def cal_red(self):
@@ -55,16 +58,17 @@ class divide:
         plt.close()
         x = range(self.frames_num)
         Series = pd.Series(self.red_average)
-        print(Series)
-        plt.figure(figsize=(16,10))
-        rol = Series.rolling(window=self.window_min).mean()
-        print(rol)
+        rol=Series.rolling(window=self.rolling_window).mean()
         peak2,_=signal.find_peaks(-rol)
-        plt.plot(peak2, rol[peak2], "x", color='black', label='red_average')
-        plt.plot(peak, self.red_average[peak], "x", color='r', label='red_average')
-        plt.plot(x, rol, color='b')
+        s_list = []
+        for i in peak2:
+            s_list.append(self._get_closest_point(i))
+        plt.plot(s_list, self.red_average[s_list], "x", color='r', label='red_average')
+        # plt.plot(peak, self.red_average[peak], "x", color='r', label='red_average')
+        # plt.plot(range(self.frames_num-self.distance), rol[self.distance:self.frames_num], color='b')
         # plt.plot( range(self.frames_num-self.window_min), rol[self.window_min-1:-1], color='b')
-        plt.plot(x,self.red_average,"--",color='g')
+        plt.plot(x,self.red_average,color='g')
+        # plt.plot(self.peak,self.red_average[self.peak],"x",color="black")
         plt.show()
 
     def print(self):
@@ -99,20 +103,23 @@ class divide:
     #     else:
     #         return index+1
 
+    def _get_closest_point(self,index):
+        index_temp=index-self.distance
+        temp=int(self.window_min/2)
+        for i in range(index_temp-temp,index_temp+temp+1):
+            if i in self.peak:
+                return i
+        return index
 
     def get_slots(self):
+        print(self.peak)
+        pump_frames=0
+        Series = pd.Series(self.red_average)
+        rol = Series.rolling(window=self.rolling_window).mean()
+        peak, _ = signal.find_peaks(-rol)
         s_list = []
-        DN_list=[]
-        peak, _ = signal.find_peaks(-self.red_average)
-        times=0
         for i in peak:
-            # print("this is peak")
-            # print(i)
-            if times%2==0:
-                s_list.append(i)
-            else:
-                DN_list.append(i)
-            times=times+1
+            s_list.append(self._get_closest_point(i))
         slots = []
         score_sum = 0
         for i in range(len(s_list) - 1):
@@ -122,41 +129,48 @@ class divide:
                 # print(str(j)+" "+str(s_list[i])+" "+str(s_list[i+1])+" ")
                 temp_list.append(self.img_list[j])
             slot = SLOT(s_list[i + 1] - s_list[i] + 1, temp_list, average, self.fps)
-            slot.DN_index=DN_list[i]
             # slot.show()
-            # slot.get_Systolic_DiastolicFeature()
-            if slot.Hz >= 0.3 and slot.Hz <= 10.0:
+            if slot.slot_size >= self.window_min and slot.slot_size <= self.window_max:
+            # if slot.Hz > (1/self.heart_pump_max) and slot.Hz <(1/self.heart_pump_min):
+                pump_frames=pump_frames+slot.slot_size
+                slot.get_Systolic_DiastolicFeature()
+                slot.get_Non_fiducialFeature()
                 score_sum = score_sum + slot.score
                 slots.append(slot)
-        return slots, len(s_list) - 1, score_sum / (len(s_list) - 1)
+        return slots, len(slots) , score_sum / len(slots),pump_frames
 
     # def get_slots(self):
-    #     peak,_=signal.find_peaks(-self.red_average)
-    #     index = 0
-    #     cont=0
+    #     pump_frames = 0
+    #     b, a = signal.butter(1,1, btype='highpass', fs=self.frames_num)
+    #     sf=signal.lfilter(b,a,self.red_average)
+    #     peak, _ = signal.find_peaks(-sf)
+    #     x=range(self.frames_num)
+    #     plt.close()
+    #     plt.plot(x,sf,color='r')
+    #     plt.plot(peak,sf[peak],"x",color="black")
+    #     plt.plot(x,self.red_average,"--",color='g')
+    #     plt.show()
     #     s_list = []
-    #     index = self.get_slot(index)
-    #     while index < self.frames_num:
-    #         s_list.append(index)
-    #         # print(str(index) + '均值为：' + str(self.red_average[index]))
-    #         temp = self.get_slot(index+1)
-    #         while (temp-index)<self.window_min:
-    #             temp=self.get_slot(temp+1)
-    #         index=temp
+    #     for i in peak:
+    #         s_list.append(i)
     #     slots = []
-    #     score_sum=0
+    #     score_sum = 0
     #     for i in range(len(s_list) - 1):
     #         average = self.red_average[s_list[i]:s_list[i + 1] + 1]
     #         temp_list = []
-    #         for j in range(s_list[i], s_list[i+1] + 1):
+    #         for j in range(s_list[i], s_list[i + 1] + 1):
+    #             # print(str(j)+" "+str(s_list[i])+" "+str(s_list[i+1])+" ")
     #             temp_list.append(self.img_list[j])
-    #         slot = SLOT(s_list[i + 1] - s_list[i] + 1, temp_list, average,self.fps)
+    #         slot = SLOT(s_list[i + 1] - s_list[i] + 1, temp_list, average, self.fps)
     #         # slot.show()
-    #         # slot.get_Systolic_DiastolicFeature()
-    #         if slot.Hz>=0.3 and slot.Hz<=10.0:
-    #             score_sum=score_sum+slot.score
+    #         if slot.slot_size >= self.window_min and slot.slot_size <= self.window_max:
+    #             # if slot.Hz > (1/self.heart_pump_max) and slot.Hz <(1/self.heart_pump_min):
+    #             pump_frames = pump_frames + slot.slot_size
+    #             slot.get_Systolic_DiastolicFeature()
+    #             slot.get_Non_fiducialFeature()
+    #             score_sum = score_sum + slot.score
     #             slots.append(slot)
-    #     return slots,len(s_list) - 1,score_sum/(len(s_list) - 1)
+    #     return slots, len(slots), score_sum / len(slots), pump_frames
 
     def get_slot(self, index):
         slot_average = -self.red_average[index:index + self.window_max]
